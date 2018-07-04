1ece0187595e14def6b808d6e0a88802e13404da
miki208 <miloss208@gmail.com>
2018-07-04 18:52:27 +0200

    Add classify.py.

--- classify.py
+++ classify.py
@@ -1,16 +1,19 @@
 from sklearn.externals import joblib
+from features import compute_features #postojeci kod koji je koriscen za izdvajanje feature-a, uz male izmene (utils.py takodje)
 
 import pandas as pd
 
-def extract_features(filename, model_name):
-	pass
+from sys import argv
+
+def extract_features(filename):
+	return joblib.load('scalers/scaler.pkl').transform(compute_features(filename).values.reshape(1, -1))
 
 if len(argv) != 3:
 	print('Usage: python classify.py {file} {model}')
 	exit()
 
 #extract features
-instance = extract_features(argv[1], argv[2])
+instance = extract_features(argv[1])
 
 #load model
 model = joblib.load(argv[2])
